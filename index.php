2681ec76a6a760275810cf69f6e89ac5146edbc2
Akashdeep Gupta <61601588+Akashdeep-47@users.noreply.github.com>
2021-09-03 04:10:04 +0530

    Deleting databse credentials

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
 $host = 'db';
-$user = 'akashdeep';
-$password = 'akpass';
-$db = 'test_db';
+$user = 'root';
+$password = 'root';
 
-$conn = new mysqli($host, $user, $password, $db);
+$conn = new mysqli($host, $user, $password);
 
 if($conn->connect_error){
 	die('Connection failed: ' . $conn->connect_error);
